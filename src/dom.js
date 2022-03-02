70b7ddc3658fbaedd1957704d8345847120bf632
xjy <xjy1907@163.com>
2022-03-02 15:39:19 +0800

    重写dom.style函数，实现重载。

--- src/dom.js
+++ src/dom.js
@@ -4,9 +4,23 @@ window.dom = {
         //const divList = dom.find('.red') // 获取多个 div.red 元素
         return (scope || document).querySelectorAll(selector);
     },
-    style(node, key, value) {
-        node.style[key] = value;
-    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+          // dom.style(div, 'color', 'red')
+          node.style[name] = value
+        } else if (arguments.length === 2) {
+          if (typeof name === "string") {
+            // dom.style(div, 'color')
+            return node.style[name]
+          } else if (name instanceof Object) {
+            // dom.style(div, {color: 'red'})
+            const object = name
+            for (let key in object) {
+              node.style[key] = object[key]
+            }
+          }
+        }
+      },
     each(nodeList, fn){
         //dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
         for(let i=0; i<nodeList.length; i++) {
